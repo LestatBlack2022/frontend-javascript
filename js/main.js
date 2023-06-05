59038aeff5463ec653889e50d7f3e20aca1ea708
Mario Ivan Moreno <msmq16@hotmail.com>
2023-06-05 12:54:06 -0600

    Se agrega el menu-mobile evento de ocultamiento por media-queries en css y clic por medio de JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-const emailMenu = document.querySelector('.navbar-email')
+const emailMenu   = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const mobileMenu  = document.querySelector('.mobile-menu')
+const menuHamIcon = document.querySelector('.menu')
 
 emailMenu.addEventListener('click', toggleDesktopMenu)
+menuHamIcon.addEventListener('click', toogleMobileMenu)
 
 function toggleDesktopMenu(){
    desktopMenu.classList.toggle('inactive')
+}
+
+function toogleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')     
 }
